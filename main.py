85efb99a37f41f843cc957a2933cf3736402d948
JTS-FIN <jarkko@lauspalo.com>
2013-01-05 02:22:06 +0200

    -@major Simple structure for runnin logic: update() and draw()

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pygame
+import time
 
 screen = pygame.display.set_mode((640, 400))
 running = 1
- 
+fps = 60
+i = 0
+
+""" Logic for making another step in game """
+def update():
+	global updateStartTime, updateEndTime, fps, i
+	updateStartTime = time.time()	
+	draw()
+	i = i + 1
+	print("Frame " + str(i))
+	pygame.time.Clock().tick(fps)
+
+def draw():
+        global screen
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 while running:
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
 		running = 0
-	screen.fill((0, 0, 0))
-	pygame.display.flip()
+	update()
