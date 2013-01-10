cf3b360af1765c7eabc948ef86254a7e576b588c
JTS-FIN <jarkko@lauspalo.com>
2013-01-10 21:46:35 +0200

    -@major PauseState -> MenuState

--- statemanager.py
+++ statemanager.py
@@ -1,18 +1,20 @@
 # -*- coding: iso-8859-15 -*-
 import state
 from gamestate import *
-from pausestate import *
+from menustate import *
 
 class StateManager:
     activeState = 0
     gamestate = 0
-    pausestate = 0
+    menustate = 0
     
     def __init__(self):
         self.gamestate = GameState()
-        self.pausestate = PauseState()
-        self.activeState = self.gamestate
+        self.menustate = MenuState()
+        self.activeState = self.menustate
+    
     def getActiveState(self):
         return self.activeState
+    
     def setActiveState(self, state):
         self.activeState = state
